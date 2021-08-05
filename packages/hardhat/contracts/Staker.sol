68c6b2211d027f624307373e081189f69e589893
captnseagraves <k.s.seagraves@gmail.com>
2021-08-05 11:35:19 -0600

    execute function written

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -14,24 +14,32 @@ contract Staker {
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
-mapping(address => uint256) public balance;
-uint256 public constant threshold = 1 ether;
+  mapping(address => uint256) public balances;
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = now + 30 seconds;
 
-event Stake(address _staker, uint256 _amount); 
+  event Stake(address _staker, uint256 _amount); 
 
-function stake() public payable {
-  balance[msg.sender] = msg.value;
-  emit Stake(msg.sender, msg.value);
-}
+  function stake() public payable {
+    balances[msg.sender] = msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
 
+  function execute() public {
+    require(now >= deadline, "Not past the deadline");
+    require(address(this).balance >= threshold, "Staked ETH not past threshold");
 
+    exampleExternalContract.complete{value: address(this).balance}();
+  }
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
+  function withdrawal() public {
 
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
 
